eb61e8753c66a7a2a09de8ceab0b08b5b6e894f4
bdthinh <bdthinh@fit.hcmus.edu.vn>
2017-05-14 17:20:17 +0700

    Feat: update endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -1,16 +1,11 @@
 // eslint-disable-next-line no-console
 console.info('using backend: %s', process.env.REACT_APP_BACKEND);
 
-const BASE_URL = process.env.REACT_APP_BACKEND || '';
+const BASE_URL = process.env.REACT_APP_BACKEND || 'https://us-central1-airlala-7b1b2.cloudfunctions.net';
 
-export const ORDER_ENDPOINT = `${BASE_URL}/Front/order`;
-export const APP_SETTINGS_ENDPOINT = `${BASE_URL}/Front/settings`;
-export const AREAS_ENDPOINT = `${BASE_URL}/Front/areas`;
-export const PRODUCTS_ENDPOINT = `${BASE_URL}/Front/products`;
-export const SELECTIONS_ENDPOINT = `${BASE_URL}/Front/selection`;
-export const USER_ENDPOINT = `${BASE_URL}/Account/user`;
-
-export const ADDRESSES_URI_ENDPOINT = `${BASE_URL}/Account/details`;
-export const POST_NEW_ADDRESS_ENDPOINT = `${BASE_URL}/Account/address`;
-export const REMOVE_ADDRESS_ENDPOINT = `${BASE_URL}/Account/addressdel`;
-export const UPDATE_ACCOUNT_DETAILS = ADDRESSES_URI_ENDPOINT;
+export const CREATE_USER_ENDPOINT = `${BASE_URL}/createUser`;
+export const REQUEST_GIFTS_ENDPOINT = `${BASE_URL}/requestGifts`;
+export const REQUEST_IMAGE_LABEL_ENDPOINT = `${BASE_URL}/requestImageLabel`;
+export const REQUEST_OTP_ENDPOINT = `${BASE_URL}/requestOneTimePassword`;
+export const REQUEST_UPDATE_ACCOUNT_ENDPOINT = `${BASE_URL}/requestUpdateAccount`;
+export const VERIFY_OTP_ENDPOINT = `${BASE_URL}/verifyOneTimePassword`;
